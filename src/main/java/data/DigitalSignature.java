4a9ef5ed8390fca6b764c5e184cbfdfc849e5352
pfu99 <paufrancino99@gmail.com>
2022-12-27 22:17:30 +0100

    Data package: canvis Artur - goal and password added, rest modified.

--- src/main/java/data/DigitalSignature.java
+++ src/main/java/data/DigitalSignature.java
@@ -1,4 +1,26 @@
 package data;
 
-public class DigitalSignature {
+import exceptions.BadFormatSignatureException;
+
+final public class DigitalSignature {
+    private final String signature;
+    public DigitalSignature(String code) throws BadFormatSignatureException {
+        if (code == null) throw new NullPointerException("Digital signature code is null.");
+        this.signature = code;
+    }
+    public String getSignature() { return signature; }
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DigitalSignature dSignature = (DigitalSignature) o;
+        return signature.equals(dSignature.signature);
+    }
+    @Override
+    public int hashCode () { return signature.hashCode(); }
+    @Override
+    public String toString () {
+        return signature;
+    }
+
 }
--- src/main/java/data/SmallCode.java
+++ src/main/java/data/SmallCode.java
@@ -1,4 +1,30 @@
 package data;
 
-public class SmallCode {
+import exceptions.BadFormatSmallCodeException;
+
+final public class SmallCode {
+    private static final String CODE_REGEX = "^[0-9]+$";
+    private static final int CODE_LENGTH = 3;
+    private final String smallCode;
+    public SmallCode (String code) throws BadFormatSmallCodeException {
+        if (code == null) throw new NullPointerException("SmallCode is null.");
+        if (!code.matches(CODE_REGEX)) throw new BadFormatSmallCodeException("SmallCode should be in numeric format");
+        if (code.length() != CODE_LENGTH)
+            throw new BadFormatSmallCodeException("SmallCode should be " + CODE_LENGTH + " digits");
+        this.smallCode = code;
+    }
+    public String getSmallCode () { return smallCode; }
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SmallCode niff = (SmallCode) o;
+        return smallCode.equals(niff.smallCode);
+    }
+    @Override
+    public int hashCode () { return smallCode.hashCode(); }
+    @Override
+    public String toString () {
+        return smallCode;
+    }
 }
