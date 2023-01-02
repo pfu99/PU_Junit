284aabb6b2ff70da26af79419e51076479d6f4db
pfu99 <paufrancino99@gmail.com>
2023-01-02 12:22:14 +0100

    replace with Artur's UnifiedPlatform for the moment.

--- src/main/java/citizenmanagementplatform/UnifiedPlatform.java
+++ src/main/java/citizenmanagementplatform/UnifiedPlatform.java
@@ -1,76 +1,147 @@
 package citizenmanagementplatform;
-import exceptions.*;
+
 import data.*;
-import jdk.internal.org.objectweb.asm.util.Printer;
+import enums.Goal;
+import exceptions.*;
 import publicadministration.*;
-import enums.*;
+import services.CertificationAuthority;
+import enums.AuthenticationMethod;
+import services.GPD;
+import services.CAS;
+import services.JusticeMinistry;
+
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class UnifiedPlatform {
-    private AuthenticationMethod authMethod;
     private Citizen citizen;
+    private Goal goal;
+    private CreditCard cardData;
     private DigitalSignature digSign;
-    private Printer printer;
     private CrimConvictionsColl crimConvs;
+    private CertificationAuthority certificationAuthority;
+    private Object authMethod;
+    private GPD gpd;
+    private CAS cas;
+    private JusticeMinistry justiceMinistry;
 
-    public UnifiedPlatform(AuthenticationMethod authMethod, Printer printer) {
-        this.authMethod = authMethod;
-        this.printer = printer;
+    public UnifiedPlatform() {
+        this.citizen = null;
+        this.goal = null;
+        this.cardData = null;
+        this.digSign = null;
+        this.crimConvs = null;
     }
 
     // Input events
-    public void selectJusMin () {
+    public void selectJusMin() {
         System.out.println("Entering to Ministerio de Justicia");
     }
-    public void selectProcedures () {
+    ;
+
+    public void selectProcedures() {
         System.out.println("Selected Trámites");
     }
-    public void selectCriminalReportCertf () {
+
+    ;
+
+    public void selectCriminalReportCertf() {
         System.out.println("Procedure selected for obtaining a Criminal Report");
     }
-    public void selectAuthMethod (byte opc) {
+
+    ;
+
+    public void selectAuthMethod(byte opc) {
         this.authMethod = AuthenticationMethod.valueOf(opc);
-        System.out.println("Selected authentication method: " + this.authMethod);
+        System.out.println("Showing authentication form");
     }
-    }
-    public void enterNIFandPINobt (Nif nif, Date valDate)
+
+    ;
+
+    public void enterNIFandPINobt(Nif nif, Date valDate)
             throws NifNotRegisteredException, IncorrectValDateException,
-            AnyMobileRegisteredException, ConnectException{
-        this.authMethod.obtainPIN(nif, valDate);
+            AnyMobileRegisteredException, ConnectException {
+        if (this.certificationAuthority.sendPIN(nif, valDate))
+            System.out.println("PIN has been sent correctly");
     }
-    public void enterPIN (SmallCode pin) throws NotValidPINException,
+
+    public void enterPIN(SmallCode pin) throws NotValidPINException,
             ConnectException {
-        this.authMethod.authenticate(pin);
+        if (this.certificationAuthority.checkPIN(citizen.getNif(), pin))
+            System.out.println("PIN checked correctly");
     }
-    private void enterForm (Citizen citz, Goal goal) throws IncompleteFormException, IncorrectVerificationException,
+    private void enterForm(Citizen citz, Goal goal) throws IncompleteFormException, IncorrectVerificationException,
             ConnectException {
-        this.citizen = citz;
-        this.crimConvs = this.authMethod.checkCriminalRecords(citz, goal);
+        if (citz == null || goal == null)
+            throw new IncompleteFormException("Please fill all the required fields in the form");
+        if (!gpd.verifyData(citz, goal))
+            throw new IncorrectVerificationException("The DGP could not verify the entered information");
     }
-    private void realizePayment () {}
-    private void obtainCertificate () throws BadPathException,
-            DigitalSignatureException, ConnectException {
-        this.digSign = this.authMethod.signDoc(this.crimConvs);
+
+    private void realizePayment() {
+        System.out.println("Ready to pay");
     }
-    private void printDocument () throws BadPathException, PrintingException {
-        this.printer.print(this.crimConvs, this.digSign);
+
+    private void enterCardData(CreditCard cardD) throws IncompleteFormException, NotValidPaymentDataException,
+            InsufficientBalanceException, ConnectException {
+        if (cardD == null) {
+            throw new IncompleteFormException("Credit card information is missing");
+        }
+        CardPayment cardPayment = new CardPayment(citizen.getNif(), new BigDecimal(5));
+        if (!cas.askForApproval(cardPayment.getReference(), cardD, cardPayment.getDate(), cardPayment.getImportValue())) {
+            throw new NotValidPaymentDataException("Credit card information is incorrect");
+        }
     }
-    // The setter methods for injecting the dependences
-    public void setAuthenticationMethod (AuthenticationMethod authMethod) {
-        this.authMethod = authMethod;
+
+    private void obtainCertificate () throws BadPathException, DigitalSignatureException, ConnectException {
+        justiceMinistry.getCriminalRecordCertf(citizen, goal);
     }
-    public void setPrinter (Printer printer) {
-        this.printer = printer;
+
+    private void printDocument () throws BadPathException,
+            PrintingException{
+
     }
+
+// The setter methods for injecting the dependences
+
+    public void setCitizen(Citizen citizen){
+        this.citizen = citizen;
+    }
+
+    public void setGoal(Goal goal){
+        this.goal = goal;
+    }
+
+    public void setDigSign(DigitalSignature digSign){
+        this.digSign = digSign;
+    }
+
+    public void setCardData(CreditCard cardData){
+        this.cardData = cardData;
+    }
+
+    public void setCrimConvs(CrimConvictionsColl crimConvs){
+        this.crimConvs = crimConvs;
+    }
+
     // Other input events (not required)
-// Other internal operations (not required)
-    private void registerPayment () {
-        this.crimConvs.registerPayment();
+    public void enterCred (Nif nif, Password passw) throws
+            NifNotRegisteredException, NotValidCredException,
+            AnyMobileRegisteredException, ConnectException{
+        certificationAuthority.ckeckCredent(nif, passw);
+        //enterPIN(certificationAuthority.ckeckCredent(nif, passw));
+
     }
-    private void openDocument (DocPath path) throws BadPathException {
-        this.digSign.openDoc(path);
+
+    // Other internal operations (not required)
+    private void registerPayment () { }
+    private void openDocument (DocPath path) throws BadPathException{
+
     }
-    private void printDocument (DocPath path) throws BadPathException, PrintingException {
-        this.printer.print(path);
+    private void printDocument (DocPath path) throws BadPathException, PrintingException{
+
     }
 }
